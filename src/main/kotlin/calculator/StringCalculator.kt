e2cd37b2b3e7190da5013e04eca64f15eca0f5ef
nooblette <min9hyuk@gmail.com>
2024-11-13 21:18:52 +0900

    feat : 리팩토링 - 각 로직을 메서드 추출

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -8,27 +8,51 @@ class StringCalculator : Calculator<String?> {
 
         // 초기화
         val tokenList = tokens.split(" ")
+
+        // 연산 결화 반환
+        return compute(tokenList = tokenList)
+    }
+
+    private fun compute(tokenList: List<String>): Double {
+        // 초기화
         var result = tokenList[0].toDouble()
 
         // 주어진 문자열을 순회하며 연산
         for (i in 1..<tokenList.size step 2) {
-            if (tokenList[i] == "+") {
-                result += tokenList[i + 1].toDouble()
-            } else if (tokenList[i] == "-") {
-                result -= tokenList[i + 1].toDouble()
-            } else if (tokenList[i] == "*") {
-                result *= tokenList[i + 1].toDouble()
-            } else if (tokenList[i] == "/") {
-                if (tokenList[i + 1].toDouble() == 0.0) {
-                    throw IllegalArgumentException("0으로 나눌 수 없습니다.")
-                }
+            val sign = tokenList[i]
+            val number = tokenList[i + 1].toDouble()
 
-                result /= tokenList[i + 1].toDouble()
-            } else {
-                throw IllegalArgumentException("사칙연산 기호가 아닙니다.")
-            }
+            result = calculateByToken(result = result, sign = sign, number = number)
+        }
+        return result
+    }
+
+    private fun calculateByToken(
+        result: Double,
+        sign: String,
+        number: Double,
+    ): Double {
+        // 연산 기호별로 계산
+        if (sign == "+") {
+            return result + number
         }
 
-        return result
+        if (sign == "-") {
+            return result - number
+        }
+
+        if (sign == "*") {
+            return result * number
+        }
+
+        if (sign == "/") {
+            if (number == 0.0) {
+                throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+            }
+
+            return result / number
+        }
+
+        throw IllegalArgumentException("사칙연산 기호가 아닙니다.")
     }
 }
