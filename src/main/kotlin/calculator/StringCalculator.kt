aa2ac11cb4db97a54adab75aee6d07f607505042
nooblette <min9hyuk@gmail.com>
2024-11-13 21:05:17 +0900

    feat : 계산기 기능 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -6,6 +6,29 @@ class StringCalculator : Calculator<String?> {
             throw IllegalArgumentException("입력값이 비어있습니다.")
         }
 
-        return 0.0
+        // 초기화
+        val tokenList = tokens.split(" ")
+        var result = tokenList[0].toDouble()
+
+        // 주어진 문자열을 순회하며 연산
+        for (i in 1..<tokenList.size step 2) {
+            if (tokenList[i] == "+") {
+                result += tokenList[i + 1].toDouble()
+            } else if (tokenList[i] == "-") {
+                result -= tokenList[i + 1].toDouble()
+            } else if (tokenList[i] == "*") {
+                result *= tokenList[i + 1].toDouble()
+            } else if (tokenList[i] == "/") {
+                if (tokenList[i + 1].toDouble() == 0.0) {
+                    throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+                }
+
+                result /= tokenList[i + 1].toDouble()
+            } else {
+                throw IllegalArgumentException("사칙연산 기호가 아닙니다.")
+            }
+        }
+
+        return result
     }
 }
--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -1,5 +1,6 @@
 package calculator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -14,5 +15,23 @@ class StringCalculatorTest {
     fun `입력값이 null인 경우 예외를 던진다`() {
         assertThatThrownBy { StringCalculator().calculate(null) }
             .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 문자가 포함되는 경우 예외를 던진다`() {
+        assertThatThrownBy { StringCalculator().calculate("2 ( 3 * 4 / 2") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `0으로 나누는 경우 예외를 던진다`() {
+        assertThatThrownBy { StringCalculator().calculate("2 + 3 / 0 - 0") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `계산 성공`() {
+        assertThat(StringCalculator().calculate("2 + 3 * 4 / 2"))
+            .isEqualTo(10.0)
     }
 }
