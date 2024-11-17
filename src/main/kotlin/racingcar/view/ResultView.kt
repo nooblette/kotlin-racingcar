458509bea1bcff2e06bc711e2ea1bf82f02b6827
nooblette <min9hyuk@gmail.com>
2024-11-17 17:03:27 +0900

    refactor : 차량 이동을 나타내는 출력 기호를 ResultView로 이동, 출력 기호 상수 선언

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -5,7 +5,11 @@ import racingcar.car.Cars
 class ResultView {
     fun printResult(cars: Cars) {
         // 자동차들의 이동 거리 출력
-        cars.list.forEach { car -> println(car.result) }
+        cars.list.forEach { car -> println(PRINT_EXPRESSION.repeat(car.distance)) }
         println()
+    }
+
+    companion object {
+        private const val PRINT_EXPRESSION = "-"
     }
 }
