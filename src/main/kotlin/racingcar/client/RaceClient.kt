0fe5cda11f3cbc6a250f47e1be2877328617f1f1
nooblette <min9hyuk@gmail.com>
2024-11-18 21:31:42 +0900

    feat : 자동자 이름 목록을 입력받는 InputView.inputCarNames 메서드 추가

--- src/main/kotlin/racingcar/client/RaceClient.kt
+++ src/main/kotlin/racingcar/client/RaceClient.kt
@@ -9,8 +9,9 @@ import racingcar.view.ResultView
 
 class RaceClient {
     fun startRace() {
-        // 자동차 대수 입력 받기
-        val carCount = InputView.inputCarCount()
+        // 자동차 이름 입력 받기
+        val carNames = InputView.inputCarNames()
+        val carCount = carNames.size
 
         // 시도 횟수 입력 받기
         val tryTime = InputView.inputTryTime()
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -7,6 +7,12 @@ object InputView {
         return readlnOrNull()?.toInt() ?: 0
     }
 
+    fun inputCarNames(): List<String> {
+        // 자동차 대수 input 받기
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return (readlnOrNull() ?: "").split(",")
+    }
+
     fun inputTryTime(): Int {
         // 시도 횟수 input 받기
         println("시도할 횟수는 몇 회인가요?")
