dd6c19fa6090f1a9bc333175c26d71d18f6a4489
nooblette <min9hyuk@gmail.com>
2024-11-18 22:03:48 +0900

    refactor : indent depth(2 미만), 메서드 길이(15라인 이하) 리팩토링

--- src/main/kotlin/racingcar/client/RaceClient.kt
+++ src/main/kotlin/racingcar/client/RaceClient.kt
@@ -10,25 +10,18 @@ import racingcar.view.ResultView
 
 class RaceClient {
     fun startRace() {
-        // 자동차 이름 입력 받기
-        val carNames = InputView.inputCarNames()
-        val carCount = carNames.size
+        // 자동차 이름 입력받아 Cars 객체 생성
+        val inputCars = getInputCars()
 
-        // 입력받은 carCount만큼 자동차 목록을 갖는 race 객체 생성
-        val race = Race(cars = Cars(list = carNames.map { carName -> Car(name = carName) }))
+        // 입력받은 이름으로 생성된 자동차 목록으로 Race 객체 생성
+        val race = Race(cars = inputCars)
         val raceService = RaceService(race)
 
         // 시도 횟수 입력 받기
         val tryTime = InputView.inputTryTime()
 
         // 경주 서비스 실행
-        ResultView.printStart()
-        val raceResult =
-            raceService.execute(
-                tryTime = tryTime,
-                onResult = ResultView::printResult,
-                randomNumbers = List(tryTime) { RandomNumbers(carCount) },
-            )
+        val raceResult = executeRace(tryTime, race.cars.list.size, raceService::execute)
 
         // 경주 결과 중 우승자를 찾는다.
         ResultView.printWinner(
@@ -37,5 +30,23 @@ class RaceClient {
                 .list
                 .joinToString { it.name },
         )
+    }
+
+    private fun getInputCars(): Cars {
+        // 자동차 이름 입력 받기
+        val carNames = InputView.inputCarNames()
+
+        // 입력받은 자동차 이름들로 Cars 객체 생성
+        return Cars(list = carNames.map { carName -> Car(name = carName) })
+    }
+
+    // 자동차 경주 실행
+    private fun executeRace(
+        tryTime: Int,
+        carCount: Int,
+        execute: (Int, (Cars) -> Unit, List<RandomNumbers>) -> Race,
+    ): Race {
+        ResultView.printStart()
+        return execute(tryTime, ResultView::printResult, List(tryTime) { RandomNumbers(carCount) })
     }
 }
